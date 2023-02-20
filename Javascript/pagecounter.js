73fa2812fa35bff5ae3a699781f9b111c5438fe6
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-02-19 22:33:26 -0700

    Update pagecounter.js

--- Javascript/pagecounter.js
+++ Javascript/pagecounter.js
@@ -1,10 +1,15 @@
 var count = 0;
-var countSpan = document.getElementById("count");
+  var countSpan = document.getElementById("count");
 
-function updateCounter() {
-  count++;
-  countSpan.textContent = count;
-}
+  function updateCounter() {
+    count++;
+    countSpan.textContent = count;
+  }
 
-// Call the updateCounter function when the page loads
-window.onload = updateCounter;
+  // Call the updateCounter function when the page loads
+  window.onload = updateCounter;
+
+  // Call the updateCounter function when the DOM is ready
+  document.addEventListener("DOMContentLoaded", function() {
+    updateCounter();
+  });
