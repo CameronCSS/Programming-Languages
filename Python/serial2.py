ca86da9f2d59b89943562c1106e0077fa4a0a101
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-05-22 16:27:40 -0600

    Update serial2.py

--- Python/serial2.py
+++ Python/serial2.py
@@ -9,14 +9,14 @@ from win32com import client
 
 file_paths = [] 
 
+target_columns = ['Fridge', 'Range', 'Microwave', 'Dishwasher', 'Washer', 'Dryer'] 
+
 def close_match(col_name, column_list): 
     best_match = difflib.get_close_matches(col_name, column_list, n=1, cutoff=0.6) 
     return best_match[0] if best_match else col_name 
 
 def process_data(): 
     combined_df = pd.DataFrame()
-
-    target_columns = ['Fridge', 'Range', 'Microwave', 'Dishwasher', 'Washer', 'Dryer'] 
 
     for file_path in file_paths: 
         df = pd.read_excel(file_path) 
@@ -35,10 +35,14 @@ def process_data():
     combined_df = combined_df.sort_values(by='unit', ascending=True) 
     combined_df = combined_df.reset_index(drop=True)
     combined_df = combined_df.rename(columns={'unit': 'Unit'})
+    
+    return combined_df
 
+def save_data():
+    combined_df = process_data()
 
     if not combined_df.empty: 
-        output_file = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile="Processed Serials") 
+        output_file = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile="Processed Serial Numbers") 
         if output_file: 
             combined_df.to_excel(output_file, index=False)
             excel = client.Dispatch("Excel.Application")
@@ -55,6 +59,21 @@ def process_data():
     else: 
         messagebox.showwarning("Warning", "No files selected.")
 
+def preview_data(): 
+    combined_df = process_data()
+
+    for i in preview_tree.get_children():
+        preview_tree.delete(i)
+
+    if not combined_df.empty:
+        for column in combined_df.columns:
+            preview_tree.heading(column, text=column)
+
+        for index, row in combined_df.iterrows():
+            preview_tree.insert('', 'end', values=list(row))
+
+        preview_tree.pack()
+
 def pick_files(): 
     files = filedialog.askopenfilenames() 
     if files: 
@@ -63,18 +82,18 @@ def pick_files():
 
 def process_files(): 
     if file_paths: 
-        process_data() 
+        save_data() 
     else: 
         messagebox.showwarning("Warning", "No files selected.") 
 
 root = tk.Tk()
 root.title("Serial Number Processing")
 
-root.geometry("350x250")
+root.geometry("1500x800")
 root.configure(background='darkgray')
 
 frame = tk.Frame(root, bg='darkgray')
-frame.place(relx=0.5, rely=0.5, anchor='center')
+frame.place(relx=0.5, rely=0.3, anchor='center')
 
 button_style = ttk.Style()
 button_style.configure('TButton', font=('calibri', 11, 'bold'), borderwidth='4')
@@ -84,5 +103,17 @@ button_pick.pack(side="left", padx=(0, 20))
 
 button_process = ttk.Button(frame, text="Process and Save", command=process_files)
 button_process.pack(side="right")
+
+button_preview = ttk.Button(frame, text="Preview Data", command=preview_data)
+button_preview.pack(side="bottom", padx=(0, 20))
+
+# Create a new frame for the Treeview
+preview_frame = tk.Frame(root, bg='darkgray')
+preview_frame.place(relx=0.5, rely=0.6, anchor='center')
+
+preview_tree = ttk.Treeview(preview_frame, columns=target_columns + ['Unit'], show='headings', height=10)
+
+for column in target_columns + ['Unit']:
+    preview_tree.heading(column, text=column)
 
 root.mainloop()
