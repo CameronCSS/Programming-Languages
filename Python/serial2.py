087ec0913d0b67020cab9eb670661e53387804b5
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-05-22 22:43:31 -0600

    Update serial2.py

--- Python/serial2.py
+++ Python/serial2.py
@@ -1,63 +1,50 @@
-## pyinstaller --noconsole serials.py --onefile --hidden-import openpyxl.cell._writer --hidden-import win32com --hidden-import win32com.client
+## pyinstaller --noconsole serials.py --onefile --hidden-import openpyxl.cell._writer
 
-import tkinter as tk 
-from tkinter import filedialog, messagebox, ttk 
-import pandas as pd 
-import difflib 
-import openpyxl
-from win32com import client
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import pandas as pd
+import difflib
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
 
-file_paths = [] 
+file_paths = []
+target_columns = ['Unit', 'Fridge', 'Range', 'Microwave', 'Dishwasher', 'Washer', 'Dryer']
 
-target_columns = ['Fridge', 'Range', 'Microwave', 'Dishwasher', 'Washer', 'Dryer'] 
+def close_match(col_name, column_list):
+    best_match = difflib.get_close_matches(col_name, column_list, n=1, cutoff=0.6)
+    return best_match[0] if best_match else col_name
 
-def close_match(col_name, column_list): 
-    best_match = difflib.get_close_matches(col_name, column_list, n=1, cutoff=0.6) 
-    return best_match[0] if best_match else col_name 
-
-def process_data(): 
+def process_data():
     combined_df = pd.DataFrame()
 
-    for file_path in file_paths: 
-        df = pd.read_excel(file_path) 
+    for file_path in file_paths:
+        df = pd.read_excel(file_path)
         df.columns = [col.lower() for col in df.columns]
-        df.columns = [close_match(col, target_columns) for col in df.columns] 
+        df.columns = [close_match(col, target_columns) for col in df.columns]
 
-        for col in target_columns: 
+        for col in target_columns:
             if col in df.columns:
-                df[col] = df[col].astype(str).str.upper().str.replace('-', '').str.replace(',', '').str.replace('.', '').str.replace(' ', '').str.replace('AND', 'N').str.replace('IS', '').str.replace('ARE', 'R').str.replace('IF', 'F').str.replace('SEA', 'C').str.replace('FP', 'FB') 
-
-        if 'unit' in df.columns:
-            df['unit'] = df['unit'].astype(int) 
+                df[col] = df[col].astype(str).str.upper().str.replace('-', '').str.replace(',', '').str.replace('.', '').str.replace(' ', '').str.replace('AND', 'N').str.replace('IS', '').str.replace('ARE', 'R').str.replace('IF', 'F').str.replace('SEA', 'C').str.replace('FP', 'FB').str.replace('#', '').str.replace(':', '')
 
         combined_df = pd.concat([combined_df, df])
 
-    combined_df = combined_df.sort_values(by='unit', ascending=True) 
+    combined_df = combined_df.sort_values(by='Unit', ascending=True)
     combined_df = combined_df.reset_index(drop=True)
-    combined_df = combined_df.rename(columns={'unit': 'Unit'})
-    
-    return combined_df
 
+    return combined_df
 
 def save_data():
     combined_df = process_data()
 
-    if not combined_df.empty: 
-        output_file = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile="Processed Serial Numbers") 
-        if output_file: 
+    if not combined_df.empty:
+        combined_df.drop_duplicates(inplace=True)
+        output_file = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile="Processed Serial Numbers")
+        if output_file:
+            combined_df.replace({pd.NA: 'N/A', 'NAN': 'N/A'}, inplace=True)
             combined_df.to_excel(output_file, index=False)
-            excel = client.Dispatch("Excel.Application")
-            workbook = excel.Workbooks.Open(output_file)
-            workbook.Worksheets[0].Columns.AutoFit()
-            output_pdf_file = filedialog.asksaveasfilename(defaultextension=".pdf", initialfile="Serials")
-
-            if output_pdf_file:
-                workbook.Worksheets[0].ExportAsFixedFormat(0, output_pdf_file)
-                workbook.Close(SaveChanges=0)
-                excel.Application.Quit()
-
-                messagebox.showinfo("Success", "Processing completed successfully!") 
-    else: 
+            auto_fit_columns(output_file)
+            messagebox.showinfo("Success", "Processing completed successfully!")
+    else:
         messagebox.showwarning("Warning", "No files selected.")
 
 def preview_data(): 
@@ -67,8 +54,15 @@ def preview_data():
         preview_tree.delete(i)
 
     if not combined_df.empty:
+        combined_df = combined_df[['Unit', 'Fridge', 'Range', 'Microwave', 'Dishwasher', 'Washer', 'Dryer']]
+
+        preview_tree["columns"] = combined_df.columns.tolist()
+
         for column in combined_df.columns:
-            preview_tree.heading(column, text=column)
+            if column.lower() == 'unit':
+                preview_tree.heading(column, text='Unit')
+            else:
+                preview_tree.heading(column, text=column)
 
         for index, row in combined_df.iterrows():
             preview_tree.insert('', 'end', values=list(row))
@@ -88,6 +82,24 @@ def process_files():
         save_data() 
     else: 
         messagebox.showwarning("Warning", "No files selected.") 
+
+def auto_fit_columns(file_path):
+    wb = load_workbook(file_path)
+    ws = wb.active
+    for column in ws.columns:
+        max_length = 0
+        column = [cell for cell in column]
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2) * 1.2
+        col_letter = get_column_letter(column[0].column)
+        ws.column_dimensions[col_letter].width = adjusted_width
+    wb.save(file_path)
+    wb.close()
 
 root = tk.Tk()
 root.title("Serial Number Processing")
