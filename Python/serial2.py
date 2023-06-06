699f934d95075f8fa14f9cf5dfb6c19db6031550
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-06-06 10:57:55 -0600

    Updated code to include error handling

--- Python/serial2.py
+++ Python/serial2.py
@@ -1,5 +1,8 @@
 ## pyinstaller --noconsole serials.py --onefile --hidden-import openpyxl.cell._writer
 
+## pyinstaller --windowed serials.py --onefile --hidden-import openpyxl.cell._writer
+
+
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 import pandas as pd
@@ -8,12 +11,14 @@ from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 from pyhtml2pdf import converter
 import os
+import threading
 
 file_paths = []
 target_columns = ['Unit', 'Fridge', 'Range', 'Microwave', 'Dishwasher', 'Washer', 'Dryer']
 tab_names = ["Preview", "Details"]
-data_loaded_label = None
-
+data_loaded_label1 = None
+data_loaded_label2 = None
+processing_label = None
 
 def close_match(col_name, column_list):
     best_match = difflib.get_close_matches(col_name, column_list, n=1, cutoff=0.6)
@@ -23,7 +28,14 @@ def process_data():
     combined_df = pd.DataFrame()
 
     for file_path in file_paths:
-        df = pd.read_excel(file_path)
+        try:
+            df = pd.read_excel(file_path)
+        except FileNotFoundError:
+            messagebox.showerror("Error", f"File not found: {file_path}")
+            continue  # Skip to the next file
+        except PermissionError:
+            messagebox.showerror("Error", f"No permission to read the file: {file_path}")
+            continue  # Skip to the next file
         df.columns = [col.lower() for col in df.columns]
         df.columns = [close_match(col, target_columns) for col in df.columns]
 
@@ -39,74 +51,124 @@ def process_data():
     return combined_df
 
 def save_data():
-    combined_df = process_data()
+    try:
+        combined_df = process_data()
+        global data_loaded_label1
+        global data_loaded_label2
+        global processing_label
 
-    if not combined_df.empty:
-        combined_df.drop_duplicates(inplace=True)
-        output_file = filedialog.asksaveasfilename(defaultextension=".xlsx", initialfile="Processed Serial Numbers")
-        if output_file:
-            combined_df.replace({pd.NA: 'N/A', 'NAN': 'N/A'}, inplace=True)
-            combined_df.to_excel(output_file, index=False)
-            auto_fit_columns(output_file)
+        # Hide the old labels
+        if data_loaded_label1:
+            data_loaded_label1.pack_forget()
+        if data_loaded_label2:
+            data_loaded_label2.pack_forget()
 
-            html_file = output_file.replace(".xlsx", ".html")
-            combined_df.to_html(html_file, index=False)
-
-            # Add CSS styling
-            css = """
-            <style>
-            table {
-                border-collapse: collapse;
-                width: 100%;
-            }
+        if not combined_df.empty:
+            combined_df.drop_duplicates(inplace=True)
             
-            th, td {
-                border: 1px solid black;
-                padding: 8px;
-                text-align: center;
-            }
-            </style>
-            """
-            
-            # Read the HTML file
-            with open(html_file, 'r') as file:
-                html_content = file.read()
-
-            # Insert the CSS styling
-            html_content_with_css = css + html_content
-
-            # Write the modified HTML file
-            with open(html_file, 'w') as file:
-                file.write(html_content_with_css)
-
+            # Ask for output file name for the PDF and use it for temporary Excel and HTML files
             output_pdf_file = filedialog.asksaveasfilename(defaultextension=".pdf", initialfile="Serials")
             if output_pdf_file:
+                processing_label = tk.Label(root, text="Processing... Please Wait", fg="blue")
+                processing_label.pack(pady=10)
+                root.update()  # Force the window to update
+
+                output_file = output_pdf_file.replace(".pdf", ".xlsx")
+                html_file = output_pdf_file.replace(".pdf", ".html")
+
+                combined_df.replace({pd.NA: 'N/A', 'NAN': 'N/A'}, inplace=True)
+                combined_df.to_excel(output_file, index=False)
+                auto_fit_columns(output_file)
+
+                combined_df.to_html(html_file, index=False)
+
+                # Add CSS styling
+                css = """
+                <style>
+                table {
+                    border-collapse: collapse;
+                    width: 100%;
+                }
+                
+                th, td {
+                    border: 1px solid black;
+                    padding: 8px;
+                    text-align: center;
+                }
+                </style>
+                """
+                
+                # Read the HTML file
+                with open(html_file, 'r') as file:
+                    html_content = file.read()
+
+                # Insert the CSS styling
+                html_content_with_css = css + html_content
+
+                # Write the modified HTML file
+                with open(html_file, 'w') as file:
+                    file.write(html_content_with_css)
+
                 converter.convert(html_file, output_pdf_file)
                 messagebox.showinfo("Success", "Processing completed successfully!")
 
-                root.destroy()
-                os.remove(html_file)
-    else:
-        messagebox.showwarning("Warning", "No files selected.") 
+                root.after(1000, root.destroy)  # Delay closing the app by 1 second
+
+                # Attempt to delete the temporary files
+                def delete_files():
+                    try:
+                        if os.path.exists(html_file):
+                            os.remove(html_file)
+                        if os.path.exists(output_file):
+                            os.remove(output_file) 
+                    except Exception as e:
+                        print(f"Failed to delete files: {e}")
+
+                root.after(500, delete_files)  # Try to delete the files after 0.5 seconds
+
+        else:
+            messagebox.showwarning("Warning", "No files selected.")
+    except Exception as e:
+        # An error occurred, show an error message
+        messagebox.showerror("Error", f"An error occurred while processing the files: {str(e)}")
+        # Attempt to delete the files in case of error
+        delete_files()
+
 
 
 def pick_files():
-    global data_loaded_label
+    global data_loaded_label1
+    global data_loaded_label2
 
-    files = filedialog.askopenfilenames()
+    files = filedialog.askopenfilenames(filetypes=(("Excel Files", "*.xls *.xlsx *.xlsm"),("All Files", "*.*")))
+
+    # check if selected files are Excel files
+    for file in files:
+        if not file.lower().endswith(('.xls', '.xlsx', '.xlsm')):
+            messagebox.showwarning("Invalid file", f"Selected file {file} is not an Excel file.")
+            return  # Do not proceed with invalid files
+
     if files:
         file_paths.clear()
         file_paths.extend(files)
-        data_loaded_label1 = tk.Label(root, text="DATA LOADED. You can now save the files.", fg="red")
-        data_loaded_label1.pack(pady=10)
 
-        data_loaded_label2 = tk.Label(root, text="Program will close after completed", fg="blue")
+        # Destroy the old labels if they exist
+        if data_loaded_label1:
+            data_loaded_label1.destroy()
+        if data_loaded_label2:
+            data_loaded_label2.destroy()
+
+        # Create new labels
+        data_loaded_label1 = tk.Label(root, text="DATA LOADED", fg="red")
+        data_loaded_label1.pack(pady=10)
+        
+        data_loaded_label2 = tk.Label(root, text="You can now Save.", fg="blue")
         data_loaded_label2.pack(pady=10)
 
 
 def process_files():
     if file_paths:
-        save_data()
+        threading.Thread(target=save_data).start()
     else:
         messagebox.showwarning("Warning", "No files selected.") 
 
@@ -143,7 +205,5 @@ button_pick.pack(pady=20)
 
 button_process = tk.Button(root, text="Process and Save", command=process_files)
 button_process.pack(pady=10)
-
-data_loaded_label = tk.Label(root) # Initialize the data_loaded_label
 
 root.mainloop()
