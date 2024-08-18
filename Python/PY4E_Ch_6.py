9b73b88d72dae5d6c1b4ca85227523b9483abab2
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2024-08-18 10:50:52 -0600

    Create PY4E_Ch_6.py

---
+++ Python/PY4E_Ch_6.py
@@ -0,0 +1,19 @@
+# This is extremely simple string manipluation. 
+# I wanted to add comments that can hopefully help anyone new to learning python for the first time.
+
+# Base code provided in the chapter
+text = "X-DSPAM-Confidence:    0.8475"
+
+# Find the ':' so we can create our float number from that point
+colon = text.find(':')
+
+# start from the : and add 1 so we start at the position directly AFTER the colon
+# the second number is the position we end on. So we go to 30 position (Which is actually beyond the string. so this will just go to the end)
+string = text[space + 1: 30]
+
+# Create the float from our string we just created.  
+# Float automatically gets rid of white space so we dont need to trim anything
+number = float(num)
+
+# print out the final float number
+print(number)
