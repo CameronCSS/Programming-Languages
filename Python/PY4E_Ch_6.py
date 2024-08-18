e9e1d8f6ee0b696479029462bdff88355a4e5ab8
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2024-08-18 12:28:46 -0600

    Update PY4E_Ch_6.py

--- Python/PY4E_Ch_6.py
+++ Python/PY4E_Ch_6.py
@@ -9,7 +9,7 @@ colon = text.find(':')
 
 # start from the : and add 1 so we start at the position directly AFTER the colon
 # the second number is the position we end on. So we go to 30 position (Which is actually beyond the string. so this will just go to the end)
-string = text[space + 1: 30]
+string = text[colon + 1: 30]
 
 # Create the float from our string we just created.  
 # Float automatically gets rid of white space so we dont need to trim anything
