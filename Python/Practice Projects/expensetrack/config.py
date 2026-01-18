861401809c50e4a8c3eb90be8c20fed35e7e9071
CameronCSS <cameronseamons@gmail.com>
2026-01-17 21:28:31 -0700

    deleted current version

--- Python/Practice Projects/expensetrack/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "postgresql://postgres:postgres@db:5432/expensetrack"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "super-jwt-secret")
